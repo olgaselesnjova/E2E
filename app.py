3ce72a1a713bcbd72f217d6a0609875301a4c14a
Olga <olgaselesnjova@gmail.com>
2023-05-23 16:34:03 +0300

    added mlmodel file + scalers

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
-from flask import Flask
-#import joblib
-#import numpy
+from flask import Flask, request
+import joblib
+import numpy
 
-#MODEL_PATH = 'mlmodels/model.pkl'
-#SCALER_PATH = 'mlmodels/scaler.pkl'
+MODEL_PATH = 'mlmodels/model.pkl'
+SCALER_PATH = 'mlmodels/scaler.pkl'
+
 app = Flask(__name__)
-#model = joblib.load(MODEL_PATH)
-#scaler = joblib.load(SCALER_PATH)
+model = joblib.load(MODEL_PATH)
+sc_x = joblib.load(SCALER_X_PATH)
+sc_y = joblib.load(SCALER_Y_PATH)
 
-@app.route('/')
-#@app.route('/predict_price', methods = ['GET'])
-def hello_world():  # put application's code here
-    return 'Hello World!'
-    #args = request.args
-    #open_plan = args.get('open_plan', default=-1, type=int)
-    #rooms = args.get('rooms', default=-1, type=int)
-    #area = args.get('area', default=-1, type=float)
-    #renovation = args.get('renovation', default=-1, type=int)
+@app.route('/predict_price', methods = ['GET'])
+def predict():  # put application's code here
+    args = request.args
+    open_plan = args.get('open_plan', default=-1, type=int)
+    rooms = args.get('rooms', default=-1, type=int)
+    area = args.get('area', default=-1, type=float)
+    renovation = args.get('renovation', default=-1, type=int)
+    x = numpy.array([open_plan, rooms, area, renovation]).reshape(1,-1)
+    x = sc_x.transform(x)
+    result = model.predict(x)
+    result = sc_y.inverse_transform(result.reshape(1,-1))
 
+    return str(result[0][0])
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True, port = 5444, host = '0.0.0.0')
